2e5f536ece15c6a71d29cf2ca4a592f048d62a8a
nesar <nmartinsar@uoc.edu>
2025-04-11 00:10:08 +0200

    Actualizacion de estados.DeathState

---
+++ Assets/FSM/DeathState.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class DeathState : IEnemyState
+{
+    private EnemyController _enemy;
+    
+    private EnemyStateMachine _stateMachine;
+
+    public DeathState(EnemyController enemy, EnemyStateMachine stateMachine)
+    {
+        this._enemy = enemy;
+        this._stateMachine = stateMachine;
+    }
+
+    public void OnEnter()
+    {
+        Debug.Log("Entered Death State");
+        _enemy.StopMovement();
+        _enemy.StartAnimation("Death", true);
+        Object.Destroy(_enemy.gameObject, 2f);
+    }
+
+    public void UpdateState()
+    {
+        
+    }
+
+    public void OnExit()
+    {
+        
+    }
+}
